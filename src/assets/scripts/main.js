bef25eb263c6363f73f11795ce7ac887fc9a484a
ssanisidro <sergio.sanisidro.95@gmail.com>
2021-03-26 19:40:44 +0100

    PEC1

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,18 @@
  * Write any other JavaScript below
  */
 
-+( function() {
++(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+})();
+
+let menu_mobile = document.getElementById("header__hamburger-menu");
+
+menu_mobile.addEventListener('click', function () {
+  const header = document.getElementsByClassName('header')
+  header[0].classList.toggle("pushed");
+  const main_content = document.getElementsByClassName('main-content')
+  main_content[0].classList.toggle('main-pushed');
+  const bar = document.getElementsByClassName('bar');
+  bar[0].classList.toggle("animate");
+}, false);
